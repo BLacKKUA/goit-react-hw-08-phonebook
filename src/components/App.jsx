65e9661195925e7d2264cdef7e2fad30af5b3baf
Roman Valko <rvalko.dev@gmail.com>
2022-08-20 18:21:29 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Form from './Form/Form';
+import Filter from './Filter/Filter';
+import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
@@ -9,6 +11,7 @@ export class App extends Component {
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
+    filter: '',
   };
 
   onDeleteContact = number => {
@@ -17,7 +20,42 @@ export class App extends Component {
     }));
   };
 
+  onAddContact = (value, number) => {
+    const id = nanoid();
+    const trimmedName = value.trim();
+    if (
+      this.state.contacts.some(
+        contact =>
+          contact.name.toLocaleLowerCase() === value.toLocaleLowerCase()
+      )
+    ) {
+      window.alert(value + 'is already in contacts');
+      return;
+    }
+    this.setState(({ contacts }) => ({
+      contacts: [...contacts, { id, name: trimmedName, number }],
+    }));
+  };
+
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
   render() {
-    return <Form state={this.state} onDeleteContact={this.onDeleteContact} />;
+    const normalizedFilter = this.state.filter.toLowerCase();
+    const filteredContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+
+    return (
+      <>
+        <Form
+          state={filteredContacts}
+          onDeleteContact={this.onDeleteContact}
+          onAddContact={this.onAddContact}
+        />
+        <Filter value={this.state.filter} onChange={this.changeFilter} />
+      </>
+    );
   }
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import { nanoid } from 'nanoid';
 import Contacts from 'components/Contacts/Contacts';
 
 class Form extends Component {
   state = {
     name: '',
+    number: '',
+  };
+  onDeleteContact = number => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== number),
+    }));
   };
 
   onAddContact = e => {
     e.preventDefault();
-    const idshka = nanoid();
-    console.log(e.currentTarget.name);
+    this.props.onAddContact(this.state.name, this.state.number);
+    this.reset();
+  };
+
+  manageInput = e => {
+    this.setState({ [e.currentTarget.name]: e.currentTarget.value });
+  };
+
+  reset = () => {
     this.setState({
-      [e.currentTarget.name]: e.currentTarget.value,
+      name: '',
+      number: '',
     });
   };
 
@@ -29,11 +42,22 @@ class Form extends Component {
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
+            value={this.state.name}
+            onChange={this.manageInput}
+          />
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+            value={this.state.number}
+            onChange={this.manageInput}
           />
           <button type="submit">Add contact</button>
         </form>
         <Contacts
-          contacts={this.props.state.contacts}
+          contacts={this.props.state}
           onDelete={this.props.onDeleteContact}
         />
       </>
