b901878587548248742bdabf298fd889e22e5f32
Roman Valko <rvalko.dev@gmail.com>
2022-09-04 00:12:24 +0300

    push]

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,18 +4,18 @@ import Filter from './Filter/Filter';
 import { nanoid } from 'nanoid';
 
 export const App = () => {
-  const [filter, setFilter] = useState('');
-  const [contacts, setContacts] = useState([
+  const test = [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  ];
 
-  const onDeleteContact = number => {
-    // setContacts(prevState => ({
-    //   contacts: prevState.contacts.filter(contact => contact.id !== number),
-    // }));
+  const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useState(test);
+
+  const onDeleteContact = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
   };
 
   useEffect(() => {
@@ -25,6 +25,8 @@ export const App = () => {
   useEffect(() => {
     if (localStorage.getItem('phoneBook') !== null) {
       setContacts(JSON.parse(localStorage.getItem('phoneBook')));
+    } else {
+      setContacts(test);
     }
   }, []);
 
@@ -41,12 +43,13 @@ export const App = () => {
       return;
     }
     const contact = { id, name: trimmedName, number };
-    setContacts(contacts.push(contact));
+    setContacts(oldArray => [...oldArray, contact]);
   };
 
   const changeFilter = e => {
     setFilter(e.currentTarget.value);
   };
+
   const normalizedFilter = filter.toLowerCase();
   const filteredContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(normalizedFilter)
